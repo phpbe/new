5f8cf72f76f822264f1d2977b4e6c5ac7caeb949
liu12 <i@liu12.com>
2022-07-12 15:35:03 +0800

    be张级  2.5 ， 增加 www 目录

--- www/index.php
+++ www/index.php
@@ -4,6 +4,7 @@ $loader = require($rootPath . '/vendor/autoload.php');
 $loader->addPsr4('Be\\Data\\', $rootPath . '/data');
 
 $runtime = new \Be\Runtime\Driver\Common();
-$runtime->setRootPath(__DIR__);
+$runtime->setRootPath($rootPath);
 \Be\Be::setRuntime($runtime);
 $runtime->execute();
+
