2e6069bd339e454259dcb4ad8426c6471c24d2c9
liu12 <i@liu12.com>
2022-09-02 12:11:14 +0800

    使用软链接外挂 www 目录时 入口文件处理

--- www/index.php
+++ www/index.php
@@ -1,4 +1,6 @@
 <?php
+// 使用软链接外挂 www 目录时，使用 $_SERVER['DOCUMENT_ROOT'] 获取
+// $rootPath =  dirname($_SERVER['DOCUMENT_ROOT']);
 $rootPath = dirname(__DIR__);
 $loader = require($rootPath . '/vendor/autoload.php');
 $loader->addPsr4('Be\\Data\\', $rootPath . '/data');
